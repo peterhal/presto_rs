95439a1e8ae88db42331e3b671ad1662ec41dba1
Peter Hallam <peterhal@fb.com>
2020-03-15 13:17:26 -0700

    command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![allow(dead_code)]
+
+use std::env;
+use std::fs;
+
 mod lexing;
 
 fn main() {
-    println!("Hello world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Missing file name");
+        return;
+    }
+
+    let filename = &args[1];
+    let read_result = fs::read_to_string(filename);
+    match read_result {
+        Ok(contents) => println!("Hello world!\n{}", contents),
+        Err(e) => println!("Error reading file {}", e),
+    }
 }
