f5ae04cae7453392c8fb60c2f870dee1bc5ac2e8
Peter Hallam <peterhal@fb.com>
2020-04-02 17:06:09 -0700

    log errors

--- t.sh
+++ t.sh
@@ -1,1 +1,1 @@
-./scripts/generate_parse_trees.sh && RUST_BACKTRACE=full cargo run private/test.sql && cargo fmt && cargo run --release private/test.sql && pushd tools/generate_parse_trees && cargo fmt && popd
+./scripts/generate_parse_trees.sh && RUST_BACKTRACE=full cargo run private/test.sql 2> private/t.err && cargo fmt && cargo run --release private/test.sql && pushd tools/generate_parse_trees && cargo fmt && popd
